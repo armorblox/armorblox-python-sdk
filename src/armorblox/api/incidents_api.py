c7465a9e568a2495eb44d94cd59c6024f4baf028
Shaunak Phatak <sphatak@armorblox.com>
2022-07-25 23:21:44 +0530

    remove previous list combining incidents,token and count

--- src/armorblox/api/incidents_api.py
+++ src/armorblox/api/incidents_api.py
@@ -39,7 +39,6 @@ class IncidentsApi(BaseApi):
             next_page_token = response_json.get('next_page_token', None)
             total_count = response_json.get('total_count', 0)
             incidents = response_json.get('incidents', [])
-            incidents.append({'next_page_token': next_page_token, 'total_count': total_count})
             return incidents, next_page_token, total_count
 
     def get(self, incident_id: int):
